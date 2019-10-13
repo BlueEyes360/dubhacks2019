455a07b760df95136d81ce17c9f5204365227680
t-se-surface <tsesurface@gmail.com>
2019-10-13 05:54:29 -0700

    Wav file testing for Azure

--- src/components/Microphone/Microphone.js
+++ src/components/Microphone/Microphone.js
@@ -1,57 +1,60 @@
 import React, { Component } from 'react';
+//import WaveFile from 'wavefile';
 import $ from 'jquery';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import "./Microphone.css";
+import './Microphone.css';
+//import { func } from 'C:/Users/tsesu/AppData/Local/Microsoft/TypeScript/3.6/node_modules/@types/prop-types';
 
 class Microphone extends Component {
 
     componentDidMount() {
-        const fs = require('fs');
-        const recorder = require('node-record-lpcm16');
-        const speech = require('@google-cloud/speech'); //Import the Goolge Cloud client
-        
-        const client = new speech.SpeechClient();
-        const encoding = 'LINEAR16';
-        const sampleRateHertz = 16000;
-        const languageCode = 'BCP-47 en-US';
-        
-        var recognizeStream;
-        
-       const config = {
-            encoding: encoding,
-            sampleRateHertz: sampleRateHertz,
-            languageCode: languageCode,
-        };
-        const audio = {
-            content: fs.readFileSync(recognizeStream).toString('base64'),
-        };
+        const WaveFile = require('wavefile');        
+        var buffer = "./Test_wav/OSR_us_000_0030_8k.wav";
+        //src="./dist/wavefile.umd.js";
+        let wav = new WaveFile(buffer);
+        let wavBuffer = wav.toBuffer();
 
-        recorder
-          .record({
-            sampleRateHertz: sampleRateHertz,
-            threshold: 0,
-            verbose: false,
-            recordProgram: 'rec', // Try also "arecord" or "sox"
-            silence: '3.0',
-          })
-          .stream()
-          .on('error', console.error)
-          .pipe(recognizeStream);
-          
-          $.ajax({
-              url: "https://speech.googleapis.com/v1/speech:recognize",
-              
-              beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader("Authentication", "AIzaSyCDa5topLkHmECOXzQli39zeABA8YkP2wg");
-              },
+        //const SUB_KEY = "f1c1a1b5b421452cafb77556f5896f27";
+        //const BASE_URL = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+        //var token = "";
+        
+        /*$.ajax({
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", SUB_KEY);
+                //xhrObj.setRequestHeader("Host","https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken");
+                //xhrObj.setRequestHeader("Content-Length", 0);
+            },
+            url: BASE_URL, 
+            type: "POST",
+        })
+        .done(function(data) {
+            token = data[0];
+        })
 
-              type: "POST",
-              data:'{ "config": {"'+ config + '" }, "audio": {"' + audio + '"}'
-          })
-        console.log('Listening, press Ctrl+C to stop.');
+        const BASE_URL_TTS = "https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US";
+        var response = "";
+        var wav_url = "http://www.voiptroubleshooter.com/open_speech/american/OSR_us_000_0030_8k.wav";
+
+        $.ajax({
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","audio/wav; codecs=audio/pcm; samplerate=16000");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", SUB_KEY);
+                xhrObj.setRequestHeader("Authorization", "Bearer " + token);
+                xhrObj.setRequestHeader("Accept", "application/json");
+            },
+            url: BASE_URL_TTS, 
+            type: "POST",
+            format: "simple",
+            //body: wav_url,
+        })
+        .done(function(data) {
+            response = data.DisplayText[0];
+        })*/
+
+        console.log('response ' + wavBuffer);
     };
 
     render() {
